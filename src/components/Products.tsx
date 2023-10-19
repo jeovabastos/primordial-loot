7361b4d33425c9db1e04aa674bfac0f1551ad812
jeovabastos <jbastos.im@gmail.com>
2023-10-19 16:34:52 -0400

    Added products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -3,13 +3,48 @@ import { ProductItem } from "./ProductItem";
 export function Products({handleClickCartValue}:{handleClickCartValue:any}){
     return(
           <section className='flex flex-wrap border border-solid p-4 gap-4'>
-            <ProductItem handleClickCartValue={handleClickCartValue} source="/rpg-stock-photo/book-1.png" title="Monster Manual" price='300,00' text="Dungeons & Dragons official bestiary"/>
-            {/* <ProductItem source="/rpg-stock-photo/book-2.png" title="Player's Handbook" price="300,00" text="Dungeons & Dragons official player's manual"/>
-            <ProductItem source="/rpg-stock-photo/book-3.png" title="Dungeon Master's Guide" price="300,00" text="Dungeons & Dragons official master's guide"/>
-            <ProductItem source="/rpg-stock-photo/book-4.jpg" title="" price="" text=""/>
-            <ProductItem source="/rpg-stock-photo/book-5.jpg" title="" price="" text=""/>
-            <ProductItem source="/rpg-stock-photo/book-6.jpg" title="" price="" text=""/> */}
-
+            <ProductItem 
+              handleClickCartValue={handleClickCartValue} 
+              source="/rpg-stock-photo/book-1.png" 
+              title="Monster Manual" 
+              price='300,00' 
+              text="Dungeons & Dragons official bestiary"
+            />
+            <ProductItem
+              handleClickCartValue={handleClickCartValue} 
+              source="/rpg-stock-photo/book-2.png" 
+              title="Player's Handbook" 
+              price="300,00" 
+              text="Dungeons & Dragons official player's manual"
+            />
+            <ProductItem 
+              handleClickCartValue={handleClickCartValue}
+              source="/rpg-stock-photo/book-3.png" 
+              title="Dungeon Master's Guide" 
+              price="300,00" 
+              text="Dungeons & Dragons official master's guide"
+            />
+            <ProductItem 
+              handleClickCartValue={handleClickCartValue}
+              source="/rpg-stock-photo/book-4.jpg" 
+              title="Call of Cthulhu" 
+              price="280,00" 
+              text="Cthulhu's tabletop RPG guide"
+            />
+            <ProductItem 
+              handleClickCartValue={handleClickCartValue}
+              source="/rpg-stock-photo/book-5.jpg" 
+              title="The Witcher" 
+              price="200,00" 
+              text="Witcher's tabletop RPG guide"
+            />
+            <ProductItem 
+              handleClickCartValue={handleClickCartValue}
+              source="/rpg-stock-photo/book-6.jpg" 
+              title="Tormenta 20" 
+              price="150,00" 
+              text="Brazilian tabletop RPG"
+            />
           </section>
     )
 }
